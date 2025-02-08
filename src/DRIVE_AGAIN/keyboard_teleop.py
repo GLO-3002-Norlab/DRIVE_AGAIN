9d03ee06175b483d7e8acd6a24e433c5daeac0e9
Nicolas Lauzon <nicolas_lauzon@outlook.com>
2025-02-08 14:48:19 -0500

    accelerated motion with bs friction

--- src/DRIVE_AGAIN/keyboard_teleop.py
+++ src/DRIVE_AGAIN/keyboard_teleop.py
@@ -4,6 +4,9 @@ import numpy as np
 
 from DRIVE_AGAIN.common import Command
 
+V_X_MAX = 5.0
+V_YAW_MAX = np.pi
+
 
 class KeyboardTeleop:
     w_pressed = False
@@ -14,23 +17,42 @@ class KeyboardTeleop:
     def __init__(self):
         self.keyboard_listener = keyboard.Listener(on_press=self.on_key_press, on_release=self.on_key_release)
         self.keyboard_listener.start()
+        self.v_x = 0.0
+        self.v_yaw = 0.0
+        self.last_timestamp_ns: None | float = None
 
     def __del__(self):
         self.keyboard_listener.join()
 
-    def get_command(self) -> Command:
-        v_x = 0.0
-        v_yaw = 0.0
-        if self.w_pressed and not self.s_pressed:
-            v_x = 0.2
-        if self.s_pressed and not self.w_pressed:
-            v_x = -0.2
-        if self.a_pressed and not self.d_pressed:
-            v_yaw = np.pi / 16.0
-        if self.d_pressed and not self.a_pressed:
-            v_yaw = -np.pi / 16.0
+    def get_command(self, timestamp_ns: float) -> Command:
+        if self.last_timestamp_ns is None:
+            self.last_timestamp_ns = timestamp_ns
+            return np.array([0.0, 0.0])
 
-        return np.array([v_x, v_yaw])
+        dt_s = (timestamp_ns - self.last_timestamp_ns) * 1e-9
+        self.last_timestamp_ns = timestamp_ns
+
+        a_x = 0.0
+        a_yaw = 0.0
+        if self.w_pressed and not self.s_pressed:
+            a_x = 2.0
+        if self.s_pressed and not self.w_pressed:
+            a_x = -2.0
+        if self.a_pressed and not self.d_pressed:
+            a_yaw = np.pi / 2.0
+        if self.d_pressed and not self.a_pressed:
+            a_yaw = -np.pi / 2.0
+
+        self.v_x = self.v_x + a_x * dt_s
+        self.v_yaw = self.v_yaw + a_yaw * dt_s
+
+        self.v_x *= 0.5
+        self.v_yaw *= 0.5
+
+        self.v_x = np.clip(self.v_x, -V_X_MAX, V_X_MAX)
+        self.v_yaw = np.clip(self.v_yaw, -V_YAW_MAX, V_YAW_MAX)
+
+        return np.array([self.v_x, self.v_yaw])
 
     def on_key_press(self, key: Key | KeyCode | None):
         if key is None or isinstance(key, Key):
